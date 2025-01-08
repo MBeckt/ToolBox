36fcf77835d579c41c28af8e73c4cf609add0f8b
MBeckt <MBeckt@users.noreply.github.com>
2025-01-08 09:22:32 +0000

    55

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -74,19 +74,19 @@
             tableLayoutPanel1.Controls.Add(label2, 0, 0);
             tableLayoutPanel1.Controls.Add(SignOutButton, 2, 0);
             tableLayoutPanel1.Dock = DockStyle.Bottom;
-            tableLayoutPanel1.Location = new Point(0, 260);
+            tableLayoutPanel1.Location = new Point(0, 255);
             tableLayoutPanel1.Margin = new Padding(2);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 1;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.Size = new Size(668, 26);
+            tableLayoutPanel1.Size = new Size(668, 31);
             tableLayoutPanel1.TabIndex = 2;
             // 
             // AccessTokenSourceLabel
             // 
             AccessTokenSourceLabel.Anchor = AnchorStyles.Left;
             AccessTokenSourceLabel.AutoSize = true;
-            AccessTokenSourceLabel.Location = new Point(83, 5);
+            AccessTokenSourceLabel.Location = new Point(83, 8);
             AccessTokenSourceLabel.Margin = new Padding(0, 0, 2, 0);
             AccessTokenSourceLabel.Name = "AccessTokenSourceLabel";
             AccessTokenSourceLabel.Size = new Size(148, 15);
@@ -97,7 +97,7 @@
             // 
             label2.Anchor = AnchorStyles.Left;
             label2.AutoSize = true;
-            label2.Location = new Point(2, 5);
+            label2.Location = new Point(2, 8);
             label2.Margin = new Padding(2, 0, 0, 0);
             label2.Name = "label2";
             label2.Size = new Size(81, 15);
@@ -107,10 +107,10 @@
             // SignOutButton
             // 
             SignOutButton.Anchor = AnchorStyles.Right;
-            SignOutButton.Location = new Point(588, 3);
+            SignOutButton.Location = new Point(588, 2);
             SignOutButton.Margin = new Padding(2);
             SignOutButton.Name = "SignOutButton";
-            SignOutButton.Size = new Size(78, 20);
+            SignOutButton.Size = new Size(78, 27);
             SignOutButton.TabIndex = 2;
             SignOutButton.Text = "Sign &Out";
             SignOutButton.UseVisualStyleBackColor = true;
@@ -174,7 +174,7 @@
             GraphResultsTextBox.Name = "GraphResultsTextBox";
             GraphResultsTextBox.ReadOnly = true;
             GraphResultsTextBox.ScrollBars = ScrollBars.Both;
-            GraphResultsTextBox.Size = new Size(659, 242);
+            GraphResultsTextBox.Size = new Size(659, 236);
             GraphResultsTextBox.TabIndex = 1;
             GraphResultsTextBox.TextChanged += GraphResultsTextBox_TextChanged;
             // 
